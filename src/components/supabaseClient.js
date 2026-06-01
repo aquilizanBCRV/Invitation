5b04288ff13281885a368270195883adb36375ae
mynerva0x13 <koutayuzuki565@gmail.com>
2026-06-01 13:34:40 +0800

    update

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,11 +1,7 @@
-// // supabaseClient.js
-// import { createClient } from '@supabase/supabase-js';
-
-// const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-// const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SECRET
+
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
