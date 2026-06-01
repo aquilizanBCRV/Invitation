a5129653eb020264edf6ce30d80839cc727d8fe3
aquilizanBCRV <26jb2.vincemmacaraig@bcrvtvi.edu.ph>
2026-06-01 12:58:53 +0800

    Update supabaseClient.js

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -14,9 +14,5 @@
 
 
 import { createClient } from '@supabase/supabase-js';
- 
-const SUPABASE_URL = import.meta.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY;
- 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
- 
+
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
